0fcc7a41377732148f234929a2b6d23053825307
Bipin Lekhak <bplthegreat@gmail.com>
2020-04-05 16:58:55 +0545

    Add slider to change m

--- make_plot.py
+++ make_plot.py
@@ -3,9 +3,8 @@ import numpy as np
 
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, Slider, TextInput
+from bokeh.models import ColumnDataSource, Slider, Button
 from bokeh.plotting import figure
-
 
 np.random.seed(42)
 
@@ -23,8 +22,8 @@ line_data = ColumnDataSource(data=dict(x=x_line, y=y_line))
 pair_plot = figure(
     plot_height=400,
     plot_width=400,
-    title= "Linear Regression",
-    tools= "crosshair,pan,reset,save,wheel_zoom",
+    title="Linear Regression",
+    tools="crosshair,pan,reset,save,wheel_zoom",
     x_range=[-1, 5.5],
     y_range=[-1, np.max(y_line) * 1.25],
 )
@@ -37,5 +36,39 @@ pair_plot.line(
     color='green',
 )
 
-curdoc().add_root(pair_plot)
+# Set up widgets
+set_m = Slider(title='m', value=m, start=0, step=0.1, end=10)
+m_incrementor = Button(label='m + 0.1', button_type='primary')
+m_decrementor = Button(label='m - 0.1', button_type='primary')
+
+
+# Setup callbacks
+def change_m(attr, old, new):
+    new_m = set_m.value
+
+    x_line = np.arange(-1, 5.1, 0.1)
+    y_line = new_m * x_line
+
+    line_data.data = dict(x=x_line, y=y_line)
+
+
+set_m.on_change('value', change_m)
+
+
+def increase_m():
+    set_m.value += 0.1
+
+
+m_incrementor.on_click(increase_m)
+
+
+def decrease_m():
+    set_m.value -= 0.1
+
+
+m_decrementor.on_click(decrease_m)
+
+buttons = row(m_decrementor, set_m, m_incrementor, width_policy='fit')
+layout = column(buttons, pair_plot)
+curdoc().add_root(layout)
 curdoc().title = "LR"
